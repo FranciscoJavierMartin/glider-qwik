ea57ea5138e4f38c7d50e698619b8e2226ee52ff
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-21 23:48:36 +0200

    Trends

--- src/components/sidebars/Trends.tsx
+++ src/components/sidebars/Trends.tsx
@@ -1,5 +1,58 @@
 import { component$ } from '@builder.io/qwik';
 
+const randomize = () => Math.floor(Math.random() * 500);
+
+const trends = [
+  {
+    category: 'Sports',
+    content: 'Some team won something!',
+    glideCount: randomize(),
+  },
+  {
+    category: 'Finance',
+    content: 'Bitcoin to the moon',
+    glideCount: randomize(),
+  },
+  {
+    category: 'PC & Games',
+    content: 'New The Elders Scroll game out!',
+    glideCount: randomize(),
+  },
+  {
+    category: 'Economy',
+    content: "It's goin well...",
+    glideCount: randomize(),
+  },
+  {
+    category: 'Celebrities',
+    content: 'Some useless news!',
+    glideCount: randomize(),
+  },
+  {
+    category: 'Movies',
+    content: 'Peter Jackson as the director of new Lotr',
+    glideCount: randomize(),
+  },
+];
+
 export default component$(() => {
-  return <></>;
+  return (
+    <div class='bg-gray-800 overflow-hidden flex-it rounded-2xl'>
+      <div class='flex-it p-4'>
+        <span class='text-xl font-bold'>Trends</span>
+      </div>
+      {trends.map((trend) => (
+        <div
+          key={trend.category}
+          class='flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700'
+        >
+          <div class='flex-it'>
+            <span class='text-gray-400 text-sm'>{trend.content}</span>
+            <span class='text-lg font-bold'>{trend.category}</span>
+            <span class='text-gray-400 text-sm'>{trend.glideCount} glides</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 });
