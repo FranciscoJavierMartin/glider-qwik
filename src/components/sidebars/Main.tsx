a131ec7394d0bb83f79b1a2b209595321336c926
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-09-22 16:57:55 +0200

    Icon button for non desktop

--- src/components/sidebars/Main.tsx
+++ src/components/sidebars/Main.tsx
@@ -6,6 +6,7 @@ import {
   HiUserCircleOutline,
   HiBellOutline,
   HiMapOutline,
+  HiPencilSquareOutline,
 } from '@qwikest/icons/heroicons';
 
 export default component$(() => {
@@ -14,7 +15,7 @@ export default component$(() => {
       <div class='xl:w-80 w-20 flex-it'>
         <div class='h-full fixed flex-it top-0'>
           <div class='flex-it h-full xl:w-80 w-20 overflow-y-auto px-3 justify-between'>
-            <div class='flex-it items-start'>
+            <div class='flex-it items-center xl:items-start'>
               <div class='p-3 pt-4 xl:pb-3 pb-0 xl:text-2xl text-sm font-bold transition duration-200 hover:opacity-80'>
                 <a href='#'>
                   <h1>Glider</h1>
@@ -76,11 +77,14 @@ export default component$(() => {
               </div>
               {/* GLIDER SEND-MESSAGE BUTTON */}
               <div class='my-1 flex-it w-10/12 cursor-pointer'>
-                <div class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition'>
+                <div class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition hidden xl:flex'>
                   <div class='flex-it flex-row text-xl font-bold text-white items-start justify-center truncate duration-200'>
                     <div>Glide It</div>
                   </div>
                 </div>
+                <button class='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded-full flex-it transition h-8 w-8 flex justify-center items-center xl:hidden'>
+                  <HiPencilSquareOutline class='h-5 w-5' />
+                </button>
               </div>
             </div>
             {/* PROFILE MENU */}
