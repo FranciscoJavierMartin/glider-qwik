97c1c9aed641d35d0a0c2307da0a892da4d5b689
Francisco <javiermartincuenca@gmail.com>
2023-10-03 00:06:06 +0200

    Redirect when auth state change

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,29 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
+  useNavigate,
 } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head/router-head';
+import { getSupabaseBrowserClient } from './utils/getSupabaseClient';
 
 import './global.css';
 
 export default component$(() => {
+  const navigate = useNavigate();
+
+  useVisibleTask$(() => {
+    const supabaseClient = getSupabaseBrowserClient();
+
+    supabaseClient.auth.onAuthStateChange(async (_event, session) => {
+      if (session?.user.id) {
+        navigate('/', { forceReload: true });
+      } else {
+        navigate('/login', { forceReload: true });
+      }
+    });
+  });
   /**
    * The root of a QwikCity site always start with the <QwikCityProvider> component,
    * immediately followed by the document's <head> and <body>.
